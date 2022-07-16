3785b5a5cd171ebc87912ea4267ad292875e89ba
Alan Silva <3899850+alanwilter@users.noreply.github.com>
2022-07-16 21:42:36 +0100

    - Added more fields - Improved layout detection - 300 dpi or more at least

--- hvf_extraction_script/utilities/ocr_utils.py
+++ hvf_extraction_script/utilities/ocr_utils.py
@@ -2,55 +2,64 @@
 # ocr_utils.py
 #
 # Description:
-#	Class definition for OCR utility functions
+# 	Class definition for OCR utility functions
 #
 ###############################################################################
 
 # Import necessary packages
 
 # Import OCR packages
-import locale
-locale.setlocale(locale.LC_ALL, 'C')
-import tesserocr
-from tesserocr import PyTessBaseAPI
-from tesserocr import PSM
+# import locale
 
-# Import necessary image packages:
-from PIL import Image
+# locale.setlocale(locale.LC_ALL, "C")
+# import tesserocr
+
 
 # General purpose image functions:
 from hvf_extraction_script.utilities.image_utils import Image_Utils
+from hvf_extraction_script.utilities.regex_utils import Regex_Utils
+
+# Import necessary image packages:
+from PIL import Image
+from tesserocr import PSM, PyTessBaseAPI
 
 
 class Ocr_Utils:
-	###############################################################################
-	# CONSTANTS AND STATIC VARIABLES ##############################################
-	###############################################################################
+    ###############################################################################
+    # CONSTANTS AND STATIC VARIABLES ##############################################
+    ###############################################################################
 
-    OCR_API_HANDLE = None;
+    OCR_API_HANDLE = None
 
-	###############################################################################
-	# OCR METHODS #################################################################
-	###############################################################################
+    ###############################################################################
+    # OCR METHODS #################################################################
+    ###############################################################################
 
-	###############################################################################
-	# Given an image, preprocesses it and pulls OCR text out of it
-	# Uses pytesseract. Assumes input is in a Numpy/openCV image format
+    ###############################################################################
+    # Given an image, preprocesses it and pulls OCR text out of it
+    # Uses pytesseract. Assumes input is in a Numpy/openCV image format
     @staticmethod
-    def perform_ocr(img):
+    def perform_ocr(img, proc_img: bool = True, debug: bool = False) -> str:
 
-		# First, preprocessor the image:
-        img = Image_Utils.preprocess_image(img);
+        if proc_img:
+            # First, preprocessor the image:
+            img = Image_Utils.preprocess_image(img, debug=debug)
 
-		# Next, convert image to python PIL (because pytesseract using PIL):
-        img_pil = Image.fromarray(img);
+        # Next, convert image to python PIL (because pytesseract using PIL):
+        img_pil = Image.fromarray(img)
 
         if not Ocr_Utils.OCR_API_HANDLE:
             Ocr_Utils.OCR_API_HANDLE = PyTessBaseAPI(psm=PSM.SINGLE_COLUMN)
-            #Ocr_Utils.OCR_API_HANDLE = PyTessBaseAPI(psm=PSM.SINGLE_BLOCK)
+            # Ocr_Utils.OCR_API_HANDLE = PyTessBaseAPI(psm=PSM.SINGLE_BLOCK)
 
-        Ocr_Utils.OCR_API_HANDLE.SetImage(img_pil);
-        text = Ocr_Utils.OCR_API_HANDLE.GetUTF8Text();
+        Ocr_Utils.OCR_API_HANDLE.SetImage(img_pil)
+        text: str = Ocr_Utils.OCR_API_HANDLE.GetUTF8Text()
 
-		# Return extracted text:
-        return text;
+        if debug:
+            out = Regex_Utils.temp_out()
+            img_pil.save(f"{out}.jpg")
+            with open(f"{out}.txt", "w") as f:
+                f.writelines(text)
+
+        # Return extracted text:
+        return text
